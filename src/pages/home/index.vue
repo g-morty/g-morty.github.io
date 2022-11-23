4d73ba2e54270e8776dc4d9c9d0c262adcd01ab4
morty <g_morty@qq.com>
2022-11-23 18:12:01 +0800

    首页增加header-搜索

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,43 +3,86 @@
     <el-container>
       <el-header>
         <div class="search-box">
-          <el-input></el-input>
-          <el-button>搜索</el-button>
+          <!-- <el-input class="search-input" @change="handleSearch" v-model="searchUrl" autocomplete="on" suffix-icon="el-icon-search" placeholder="搜索" autofocus></el-input> -->
+          <el-input class="search-input" @keypress.enter.native="handleSearch" v-model="searchUrl" autocomplete="on" suffix-icon="el-icon-search" placeholder="搜索" autofocus></el-input>
         </div>
       </el-header>
-      <el-main>
+      <!-- <el-main>
         <el-button type="primary" round>
           <img src="http://www.bilibili.com/favicon.ico"></img>
           <span>{{msg}}</span>
         </el-button>
       </el-main>
-      <el-footer>Footer</el-footer>
+      <el-footer>Footer</el-footer> 
+
+    
+    /^(((ht|f)tps?):\/\/)?([^!@#$%^&*?.\s-]([^!@#$%^&*?.\s]{0,63}[^!@#$%^&*?.\s])?\.)+[a-z]{2,6}\/?/
+    /^((ht|f)tps?:\/\/)?[\w-]+(\.[\w-]+)+:\d{1,5}\/?$/
+     
+     
+      -->
     </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      msg:'qwe',
-      url: "https://www.baidu.com/s?ie=UTF-8&wd=asfs"
-    }
-  }
+      searchUrl: "",
+      msg: "qwe",
+      url: "https://www.baidu.com/s?ie=UTF-8&wd=asfs",
+    };
+  },
+  methods: {
+    handleSearch() {
+      const searchUrl = this.searchUrl;
+      // 域名正则列表
+      const regList = [
+        // 域名，非网址，不包含协议
+        /^([0-9a-zA-Z-]{1,}\.)+([a-zA-Z]{2,4})(:\d{1,5}\/?)*$/,
+        // 带端口网址
+        /^((ht|f)tps?:\/\/)?[\w-]+(\.[\w-]+)+:\d{1,5}\/?$/,
+        // 网址url
+        /^(((ht|f)tps?):\/\/)?([^!@#$%^&*?.\s-]([^!@#$%^&*?.\s]{0,63}[^!@#$%^&*?.\s])?\.)+[a-z]{2,6}\/?/,
+      ];
+      // 是否是域名
+      const isUrl = regList.some((item) => item.test(searchUrl));
+      // 如果是域名跳转至相应网址
+      if (isUrl) {
+        // 是否携带协议头
+        if (regList[0].test(searchUrl)) {
+          window.open("http://"+searchUrl);
+        } else {
+          window.open(searchUrl);
+        }
+      }
+      // 如果不是域名，打开百度搜索
+      else {
+        searchUrl.trim() !== "" &&
+          window.open("https://www.baidu.com/s?ie=UTF-8&wd=" + searchUrl);
+      }
+      // 清空输入框
+      this.searchUrl = "";
+    },
+  },
 };
 </script>
 
 <style scoped lang="less">
-  .el-header{
-    padding-top: 20px;
-    box-sizing: content-box;
-    background-color: rgb(237, 245, 244);
+.el-header {
+  padding-top: 20px;
+  padding-bottom: 10px;
+  box-sizing: content-box;
+  background-color: rgb(237, 245, 244);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .search-box {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    .search-box{
-      display: flex;
+    .search-input {
+      width: 400px;
     }
   }
-
+}
 </style>
