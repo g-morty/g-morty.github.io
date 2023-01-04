7ab75d529b73eaeea4e4659db7ffc85a31304ac1
morty <g_morty@qq.com>
2023-01-04 13:12:03 +0800

    update: 更新代理/项目启动方式

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = defineConfig({
         }
       },
       '/api': {
-        target: 'http://127.0.0.1:10080/',
+        target: 'http://127.0.0.1:20080/',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
